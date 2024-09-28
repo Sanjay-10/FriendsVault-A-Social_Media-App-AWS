0977b33cbc309f2608fd7209fe574b2c89577985
Sanjay-10 <99119329+Sanjay-10@users.noreply.github.com>
2024-09-28 01:02:14 -0400

    http

--- client/src/scenes/widgets/FriendListWidget.jsx
+++ client/src/scenes/widgets/FriendListWidget.jsx
@@ -14,7 +14,7 @@ const FriendListWidget = ( {userId} ) => {
 
     const getFriends = async () => {
         const response = await fetch(
-           `https://3.136.250.119:3001/users/${userId}/friends`,
+           `http://3.136.250.119:3001/users/${userId}/friends`,
            {
             method: "GET",
             headers: { Authorization: `Bearer ${token}`}
